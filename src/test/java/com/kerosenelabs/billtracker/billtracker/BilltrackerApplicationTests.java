141aba2f537ee4dd27fe051971d64ca1fe8312f6
hlafaille <hlafaille@proton.me>
2024-12-05 08:57:27 -0500

    first commit

---
+++ src/test/java/com/kerosenelabs/billtracker/billtracker/BilltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerosenelabs.billtracker.billtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
