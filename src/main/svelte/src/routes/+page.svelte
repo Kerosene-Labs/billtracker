56602bd078f2e1cb3d1ac49cdb51a18e0cb9ee7e
hlafaille <hlafaille@proton.me>
2024-12-19 08:23:14 -0500

    we're going to switch our frontend over to svelte

--- src/main/svelte/src/routes/+page.svelte
+++ src/main/svelte/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Button from "$lib/Button.svelte";
+</script>
+
+<div class="flex flex-col items-center justify-center h-full">
+    <Button text="Poop"></Button>
+</div>
