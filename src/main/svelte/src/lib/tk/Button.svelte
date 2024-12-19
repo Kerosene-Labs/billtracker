aaef5cad333bd9283d58d030ae538bf3367bceaa
hlafaille <hlafaille@proton.me>
2024-12-19 10:43:49 -0500

    building out login page

--- src/main/svelte/src/lib/tk/Button.svelte
+++ src/main/svelte/src/lib/tk/Button.svelte
@@ -9,7 +9,7 @@
         @apply font-semibold;
         @apply px-5 py-2 rounded-lg;
         @apply text-neutral-300;
-        @apply dark:bg-neutral-700;
+        @apply bg-neutral-700;
         @apply hover:dark:bg-neutral-600/70;
         @apply active:dark:bg-neutral-600/95;
         @apply focus:ring-2 focus:ring-neutral-400;
--- src/main/svelte/src/routes/+page.svelte
+++ src/main/svelte/src/routes/+page.svelte
@@ -7,15 +7,21 @@
 
 <div class="flex flex-col items-center justify-center min-h-full">
     <Card>
-        <div class="flex flex-col xl:flex-row gap-8">
+        <div class="grid grid-cols-1 xl:grid-cols-2 gap-12">
             <div class="flex flex-col gap-2">
-                <h2>Log In</h2>
+                <div class="flex flex-col">
+                    <h2>Log In</h2>
+                    <p class="subtitle">We're happy you're back.</p>
+                </div>
                 <LineEdit id="email" type="email" label="E-Mail"></LineEdit>
                 <LineEdit id="password" type="password" label="Password"></LineEdit>
                 <Button>Log In</Button>
             </div>
             <div class="flex flex-col gap-2 h-full justify-center">
-                <h2>Or Join Us</h2>
+                <div class="flex flex-col">
+                    <h2>Or Join Us</h2>
+                    <p class="subtitle">Join and start tracking your money.</p>
+                </div>
                 <Button on:click={() => {goto("/signup")}}>Sign Up</Button>
             </div>
         </div>
