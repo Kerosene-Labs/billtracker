4dc9c8d3b0329fba5d02755f5e698412a6d984c8
hlafaille <hlafaille@proton.me>
2024-12-23 11:34:19 -0500

    building out nav and drawer

--- src/main/svelte/src/lib/components/NavUserCard.svelte
+++ src/main/svelte/src/lib/components/NavUserCard.svelte
@@ -4,6 +4,7 @@
     import {onMount} from "svelte";
     import {ResponseError, SettingsApi} from "$lib/sdk";
     import {addToToastQueue, ToastType} from "$lib/toast";
+    import Card from "$lib/tk/Card.svelte";
 
     onMount(async () => {
         new SettingsApi(getPrivateApiConfig()).getIntroductions()
@@ -21,7 +22,8 @@
 
 </script>
 
-<div class="bg-neutral-800 p-4 flex flex-col gap-4">
+<div class="bg-neutral-800 py-8 px-4 flex flex-col gap-2">
     <h1>Hello, {firstName}.</h1>
+    <p class="text-neutral-300 font-semibold">Did you know that Octopuses have three hearts, and two of them stop beating when they swim!</p>
     <Button on:click={doLogOut}>Log Out</Button>
 </div>
