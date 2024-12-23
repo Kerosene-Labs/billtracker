c0ac45cae820696cdc5f71c48e3fcac314a96802
hlafaille <hlafaille@proton.me>
2024-12-22 21:04:13 -0500

    adding initial navbar

--- src/main/svelte/src/lib/components/BaseLayout.svelte
+++ src/main/svelte/src/lib/components/BaseLayout.svelte
@@ -4,10 +4,8 @@
     export let justifyCenter: boolean = true;
 </script>
 
-<Nav>
-    <div class="flex flex-col items-center min-h-full" class:justify-center={justifyCenter}>
-        <div class="flex flex-col  p-8 min-h-full gap-6">
-            <slot></slot>
-        </div>
+<div class="flex flex-col items-center min-h-full" class:justify-center={justifyCenter}>
+    <div class="flex flex-col  p-8 min-h-full gap-6">
+        <slot></slot>
     </div>
-</Nav>
+</div>
