57c85910dfb7a030e9e1b5ddec7518abd98084dd
hlafaille <hlafaille@proton.me>
2024-12-22 21:12:10 -0500

    adding initial navbar

--- src/main/svelte/src/lib/tk/ToastQueue.svelte
+++ src/main/svelte/src/lib/tk/ToastQueue.svelte
@@ -3,7 +3,7 @@
     import Toast from "$lib/tk/Toast.svelte";
 </script>
 
-<div class="fixed top-0 left-0 flex flex-col gap-2 p-2 z-10 drop-shadow-2xl">
+<div class="fixed top-0 left-0 flex flex-col gap-2 p-2 pt-16 z-10 drop-shadow-2xl">
     {#each $toastQueue as toastRequest, index}
         <Toast request={toastRequest} id={index}></Toast>
     {/each}
