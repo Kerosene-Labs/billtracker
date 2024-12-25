6580ba3ffdf81e67e9bbd9209ced82fbcd9d888c
hlafaille <hlafaille@proton.me>
2024-12-25 13:15:29 -0500

    kotlin update complete

--- src/main/java/com/kerosenelabs/billtracker/model/external/response/GoogleOAuthUserInfoResponse.kt
+++ src/main/java/com/kerosenelabs/billtracker/model/external/response/GoogleOAuthUserInfoResponse.kt
@@ -1,11 +1,13 @@
 package com.kerosenelabs.billtracker.model.external.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class GoogleOAuthUserInfoResponse(
     val sub: String,
     val name: String,
-    val givenName: String,
-    val familyName: String,
+    @JsonProperty("given_name") val givenName: String,
+    @JsonProperty("family_name") val familyName: String,
     val email: String,
     val picture: String,
-    val emailVerified: Boolean
+    @JsonProperty("email_verified") val emailVerified: Boolean
 )
