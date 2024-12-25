b22cf87ad9f68884acfc3e98dd096dfc3c232d86
hlafaille <hlafaille@proton.me>
2024-12-25 12:57:46 -0500

    switching lombok classes to kotlin dataclasses

--- src/main/java/com/kerosenelabs/billtracker/BilltrackerApplication.kt
+++ src/main/java/com/kerosenelabs/billtracker/BilltrackerApplication.kt
@@ -1,24 +1,22 @@
-package com.kerosenelabs.billtracker;
+package com.kerosenelabs.billtracker
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @OpenAPIDefinition(
-		servers = {
-				@Server(url = "https://billtracker.kerosenelabs.com/api/v1", description = "Production"),
-				@Server(url = "https://dev.billtracker.kerosenelabs.com", description = "Development"),
-				@Server(url = "http://localhost:8080", description = "Local")
-		}
+    servers = [
+        Server(url = "https://billtracker.kerosenelabs.com/api/v1", description = "Production"),
+        Server(url = "https://dev.billtracker.kerosenelabs.com", description = "Development"),
+        Server(url = "http://localhost:8080", description = "Local")
+    ]
 )
-@CrossOrigin()
+@CrossOrigin
 @SpringBootApplication
-public class BilltrackerApplication {
+open class BilltrackerApplication
 
-	public static void main(String[] args) {
-		SpringApplication.run(BilltrackerApplication.class, args);
-	}
-
+fun main(args: Array<String>) {
+    runApplication<BilltrackerApplication>(*args)
 }
