6ab43fc009fc6b67d62e3564f455499fcc2c466b
hlafaille <hlafaille@proton.me>
2024-12-25 21:42:49 -0500

    the expenses table is looking sweet so far

--- src/main/svelte/src/lib/components/expenses/ExpenseEventTable.svelte
+++ src/main/svelte/src/lib/components/expenses/ExpenseEventTable.svelte
@@ -21,24 +21,24 @@
     })
 </script>
 
-<div class="rounded-2xl bg-neutral-800 flex flex-col gap-2 text-center">
+<div class="rounded-2xl bg-neutral-800 flex flex-col text-center overflow-clip max-h-[20rem]">
     <!-- Header -->
     <div class="grid grid-cols-4 font-mono font-bold text-sm rounded-t-2xl bg-neutral-900/80 p-4 text-neutral-400">
         <span>Amount</span>
-        <span>Date</span>
+        <span>Occurred On</span>
         <span>Description</span>
         <span>Type</span>
     </div>
     {#if expenses === undefined}
         <!--Loading Spinner-->
-        <div class="flex w-full">
+        <div class="flex w-full justify-center p-12 scale-[180%]">
             <Spinner></Spinner>
         </div>
     {:else}
         <!--Content-->
-        <div class="flex flex-col gap-2 p-2 font-semibold text-white">
+        <div class="flex flex-col font-semibold text-white overflow-y-auto">
             {#each expenses as expense}
-                <div class="grid grid-cols-4">
+                <div class="grid grid-cols-4 hover:bg-neutral-700/10 active:bg-neutral-700/20 p-4 transition-colors border-b border-b-neutral-700/50">
                     <span>${expense.amount}</span>
                     <span>{expense.date.toDateString()}</span>
                     <span>...</span>
