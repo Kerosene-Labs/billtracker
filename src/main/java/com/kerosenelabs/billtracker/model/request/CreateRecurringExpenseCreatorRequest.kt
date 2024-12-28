9cb7f214e3d0fd07ac55d3284b4d259e49f3581e
hlafaille <hlafaille@proton.me>
2024-12-27 19:50:42 -0500

    BIL-3: ironing out recurring expenses

--- src/main/java/com/kerosenelabs/billtracker/model/request/CreateRecurringExpenseCreatorRequest.kt
+++ src/main/java/com/kerosenelabs/billtracker/model/request/CreateRecurringExpenseCreatorRequest.kt
@@ -1,11 +1,13 @@
 package com.kerosenelabs.billtracker.model.request
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Positive
 import java.math.BigDecimal
 
 data class CreateRecurringExpenseCreatorRequest(
-    @field:Positive val recursEveryDays: Int,
+    @field:Min(1) @field:Max(28) val recursEveryDays: Int,
     @field:Positive val amount: BigDecimal,
     @field:NotBlank val description: String,
 )
