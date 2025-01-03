ed789cc38984502ae62a274540f5ba5beb211599
hlafaille <hlafaille@proton.me>
2025-01-03 11:37:32 -0500

    BIL-6: removing page transitions

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import "../app.css";
   import ToastQueue from "$lib/tk/ToastQueue.svelte";
+  import { onNavigate } from "$app/navigation";
 
   let { children } = $props();
+
 </script>
 
 <ToastQueue></ToastQueue>
